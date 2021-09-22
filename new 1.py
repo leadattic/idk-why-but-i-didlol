d3e3ca23d4b22eacacc923b182909e8962f27443
leadattic_ <90087384+leadattic@users.noreply.github.com>
2021-09-22 13:59:57 +0200

    Add files via upload

---
+++ new 1.py
@@ -0,0 +1,8 @@
+import keyboard.
+import time.
+# Using time.sleep, we can dramatically decrease the amount of CPU our program.
+# uses.
+​
+hotkey = "shift + ctrl + F2"
+# Remember that the order in which the hotkey is set up is the order you.
+# need to press the keys.
